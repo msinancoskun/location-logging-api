0fbc4ebb48efea99a0dfcd96907c6f1251a2dbb7
msinancoskun <sinancoskun2345@gmail.com>
2025-02-19 14:23:51 +0300

    migration

--- prisma/migrations/20250216184154_default_shape/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Area" ALTER COLUMN "shape" SET DEFAULT 'CIRCLE';
--- prisma/migrations/20250219112340_init/migration.sql
+++ prisma/migrations/20250219112340_init/migration.sql
@@ -1,22 +1,27 @@
+-- CreateEnum
+CREATE TYPE "Shape" AS ENUM ('CIRCLE', 'SQUARE', 'RECTANGLE', 'TRIANGLE');
+
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "Area" (
     "id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+    "radiusInKilometers" DOUBLE PRECISION NOT NULL,
+    "shape" "Shape" NOT NULL DEFAULT 'CIRCLE',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Area_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Location" (
     "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
     "latitude" DOUBLE PRECISION NOT NULL,
     "longitude" DOUBLE PRECISION NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "Location_pkey" PRIMARY KEY ("id")
 );
@@ -24,32 +29,33 @@ CREATE TABLE "Location" (
 -- CreateTable
 CREATE TABLE "Log" (
     "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "areaId" TEXT NOT NULL,
-    "timeOfEntry" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "areaId" TEXT NOT NULL,
+    "locationId" TEXT NOT NULL,
 
     CONSTRAINT "Log_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Area" (
+CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "latitude" DOUBLE PRECISION NOT NULL,
-    "longitude" DOUBLE PRECISION NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Area_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+-- AddForeignKey
+ALTER TABLE "Location" ADD CONSTRAINT "Location_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Log" ADD CONSTRAINT "Log_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Log" ADD CONSTRAINT "Log_areaId_fkey" FOREIGN KEY ("areaId") REFERENCES "Area"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Log" ADD CONSTRAINT "Log_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
